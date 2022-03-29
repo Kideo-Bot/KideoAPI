2d26fce46c4ff80e52a9ca42503d3cb86ad304fc
Viazel <timeo.morsilli@gmail.com>
2022-03-29 19:32:42 +0200

    Change all API

--- DbService.js
+++ DbService.js
@@ -18,6 +18,10 @@ class DbService {
         })
     }
 
+    /**
+     *
+     * @return {Promise<string>}
+     */
     async getAllData(){
         try {
             const response = await new Promise((resolve, reject) => {
@@ -30,9 +34,70 @@ class DbService {
                 })
             })
 
-            return response;
+            return JSON.stringify(response);
 
         } catch (error) {
+            console.log(err);
+        }
+    }
+
+    /**
+     * @param options {{ServerID: string, XP: Number}};
+     * @return {Promise<boolean>};
+     */
+    async createServer(options){
+        try {
+            const response = new Promise((resolve, reject) => {
+
+                const query = `INSERT INTO test VALUES ('${options.ServerID}','${options.XP}')`;
+
+                this.con.query(query, (err, res) => {
+
+                    if(err)  reject(new Error(err.message));
+
+                    resolve(true);
+
+                })
+
+            })
+
+            if(response){
+                return response;
+            }
+
+            return false;
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    /**
+     * @param guidID {string};
+     * @return {Promise<boolean>}
+     */
+    async deleteGuildWithID(guidID){
+        try {
+            const response = new Promise((resolve, reject) => {
+
+                const query = `DELETE FROM test WHERE ServerID = '${guidID}'`;
+
+                this.con.query(query, (err, res) => {
+
+                    if(err) reject(new Error(err.message));
+
+                    resolve(true);
+
+                })
+
+            })
+
+            if(response){
+                return response
+            }
+
+            return false;
+        } catch (err) {
             console.log(err);
         }
     }
--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-const express = require("express");
-
-const server = new express();
+const http = require("http");
 
 const DbService = require("./DbService");
 
 const sql = new DbService();
 
-server.get("/api", async (req, res) => {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify(await sql.getAllData()));
-})
+const server = http.createServer((req, res) => {
 
-server.listen(5000, "0.0.0.0", () => {
-    console.log("The API is listening on http://localhost:5000/api");
+    let body = "";
+
+    req.on("data", chunk => {
+        body+=chunk;
+    }).on("end", async () => {
+
+        if(req.method === "POST"){
+            body = JSON.parse(body);
+        }
+
+        if(req.url === "/api/guild"){
+            if(req.method === "POST"){
+                if(body.ServerID !== undefined || body.XP !== undefined){
+                    if(await sql.createServer({ServerID: body.ServerID, XP: body.XP})){
+                        console.log("Server created !");
+                    }
+                }
+            }
+        }
+
+        if(req.url === "/api/deleteGuild"){
+            if(req.method === "POST"){
+                if(body.ServerID !== undefined){
+                    if(await sql.deleteGuildWithID(body.ServerID)){
+                        console.log("Server deleted !");
+                    }
+                }
+            }
+        }
+
+    })
+
+}).listen({port: 5000, host: "0.0.0.0"}, () => {
+    console.log("API started, go to http://localhost:5000/api");
 })
